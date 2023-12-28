3a08c41b96535ba8928a20cc08278a05b12880a8
thegravity98 <83178186+thegravity98@users.noreply.github.com>
2023-12-28 21:48:50 +0530

    Enhanced Code

--- contentscripts.js
+++ contentscripts.js
@@ -1,31 +1,23 @@
 setTimeout(() => {
-	var tid = null, _last = null;
-	var domain = window.location.toString();
-	var shift;
-	var isBraveSearch = false;
-	var isGoogleSearch = false;
-	var isYouAISearch = false;
-	//console.log("domain is " + domain);
-
-	if (domain.includes("google.com")) { isGoogleSearch = true; }
-	if (domain.includes("search.brave.com")) { isBraveSearch = true; }
-	if (domain.includes("you.com")) { isYouAISearch = true; }
+	let tid = null, _last = null;
+	const domain = window.location.toString();
+	const isSearchEngine = domain.includes("google.com") || domain.includes("search.brave.com") || domain.includes("you.com");
 
 	function isHtmlLink(aNode) {
 		return ((aNode instanceof HTMLAnchorElement && aNode.href) || (aNode instanceof HTMLAreaElement && aNode.href) || aNode instanceof HTMLLinkElement);
 	};
+	
+	function dbclickTab(e, shift) {
+		e.preventDefault();
+		e.stopPropagation();
+		clearTimeout(tid);
+		const newEvent = new MouseEvent("click", Object.defineProperties(e, { detail: { value: 1 }, ctrlKey: { value: true }, shiftKey: { value: shift } }));
+		newEvent.stopPropagation();
+		e.target.dispatchEvent(newEvent);
+	}
 
 	function onClick(e) {
-
-		function dbclickTab(shift) {
-			e.preventDefault();
-			e.stopPropagation();
-			clearTimeout(tid);
-			var evt = new MouseEvent("click", Object.defineProperties(e, { detail: { value: 1 }, ctrlKey: { value: true }, shiftKey: { value: shift } }));
-			evt.stopPropagation();
-			e.target.dispatchEvent(evt);
-		}
-
+		
 		if (!e.isTrusted || e.button != 0 || e.shiftKey || e.altKey || e.metaKey || e.ctrlKey) {
 			return;
 		}
@@ -35,26 +27,17 @@ setTimeout(() => {
 			console.log("isLinkNode: " + !!node);
 			if (node && node.getAttribute("href") != "#") {
 				_last = e.target;
-				var evt = new MouseEvent(e.type, e);
+				const delayEvent = new MouseEvent(e.type, e);
 				e.preventDefault();
 				e.stopPropagation();
-				tid = setTimeout(() => e.target.dispatchEvent(evt), 300);
+				tid = setTimeout(() => e.target.dispatchEvent(delayEvent), 300);
 			}
 		}
 
 		if (e.detail == 2 && _last == e.target) {
-			if (isGoogleSearch || isBraveSearch || isYouAISearch) {
-				shift = false;
-				dbclickTab(shift);
-			}
-			if (!isGoogleSearch && !isBraveSearch && !isYouAISearch) {
-				shift = true;
-				dbclickTab(shift);
-			}
+			dbclickTab(e, !isSearchEngine);
 		}
 
 	}
 	addEventListener("click", onClick, true);
 }, 0);
-
-
